be5a0aa9688a39f4ba6cbee659793a707f258e2a
Kathryn Anne S Tan <kathryn@fly.io>
2024-03-13 19:37:17 +0300

    Revert to previous view config using the systems temp dir, but, include a subdirectory for the project so it can be deletable without affecting other files in the systems temp directory

--- config/view.php
+++ config/view.php
@@ -4,7 +4,8 @@ return [
     'paths' => [
         resource_path('views'),
     ],
-   'compiled' => \Phar::running()
-       ? getcwd().'/storage/framework/views'
-       : env('VIEW_COMPILED_PATH', realpath(storage_path('framework/views'))),
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        sys_get_temp_dir().'/dockerfile-laravel-view', // Please rm -r '/tmp/dockerfile-laravel-view' to delete cached views
+    )
 ];
