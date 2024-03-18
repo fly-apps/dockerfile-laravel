0138e5aca1e45a45c8d93b650852c1739ff1a253
Kathryn Anne S Tan <kathryn@fly.io>
2024-03-19 02:01:40 +0300

    Include .fly directory as templates to create when fly.toml file is found, include .fly files as blade templates, create a Scanner service class to contain methods that scan for stuff like templates, and create File service class to contain methods for file-related handling

---
+++ app/Services/File.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Services;
+
+class File
+{
+    /**
+     * Generate a file with content
+     * 
+     * @param string output - the path to the new file
+     * @param string result - the content of the new file
+     */
+    public function createFile( $output, $result )
+    {
+        // Create the directory the file is supposed to be located in 
+        if( strpos($output, '/') ){
+
+            // Everything before the last none item in the array is the path to the file
+            $parts = explode('/',$output); 
+            $end = end( $parts );
+            $pathToFile = rtrim( $output, $end );
+          
+            // Recursively create directories
+            if( !file_exists($pathToFile) )
+                mkdir( $pathToFile, 0755, true );
+
+        }
+       
+        // Create the file, finally!
+        return file_put_contents($output, implode("\n", $result) . "\n");
+    }
+}
