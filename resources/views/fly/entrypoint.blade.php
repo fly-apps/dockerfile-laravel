c02908adcf983400cda9506c395aefcb8a9db2b9
Kathryn Anne Tan <ktan.software.developer@gmail.com>
2025-07-25 19:30:14 +0300

    Chown specific folders related to cache commands!

--- resources/views/fly/entrypoint.blade.php
+++ resources/views/fly/entrypoint.blade.php
@@ -7,6 +7,11 @@ for f in /var/www/html/.fly/scripts/*.sh; do
     bash "$f" -e
 done
 
+# By default, the .fly/scripts/caches.sh is going to get executed above
+# So, set proper permissions for generated files from the caching commands ran in caches.sh:
+chown -R www-data:www-data /var/www/html/boostrap
+chown -R www-data:www-data /var/www/html/storage/framework
+
 if [ $# -gt 0 ]; then
     # If we passed a command, run it as root
     exec "$@"
